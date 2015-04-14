fc616ce294cd14f5d2c69ad7359689c1f3e8cadf
Prelang Builder <builder@prelang.com>
2015-04-14 20:54:11 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Outstanding' to allow for create, update

--- app/admin/outstanding.rb
+++ app/admin/outstanding.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Outstanding do
+  permit_params :title, :description
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
