0684b6ddb3521854c19369bde02143632543a53c
Prelang Builder <builder@prelang.com>
2015-04-14 20:54:06 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Guide' to allow for create, update

--- app/admin/guide.rb
+++ app/admin/guide.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Guide do
+  permit_params :title, :ingame_title, :content
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
