c0863b944352c8c8205a4c17023c265fd5615139
Prelang Builder <builder@prelang.com>
2015-04-14 20:54:19 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Identifier' to allow for create, update

--- app/admin/identifier.rb
+++ app/admin/identifier.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Identifier do
+  permit_params :path
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
