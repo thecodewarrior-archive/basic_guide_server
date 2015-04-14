e5ae3f6c3276b8d8d4a91a5ef21bc2e1c25de915
Prelang Builder <builder@prelang.com>
2015-04-14 20:54:34 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'GuideImage' to allow for create, update

--- app/admin/guide_image.rb
+++ app/admin/guide_image.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register GuideImage do
+  permit_params :title, :description, :aspect_ratio
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
